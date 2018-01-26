41d1c5345d412cf08757db1fb0ce7408f6f49d25
KyleMondina <kylemondina@gmail.com>
2018-01-25 21:51:25 -0800

    added the matching system

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,20 +3,20 @@ $(document).ready(function() {
 
     class Profile
     {     
-        constructor(imgsrc,name,preference)
+        constructor(imgsrc,name,language)
         {
             this.imagesrc = imgsrc;
             this.name = name;
-            this.preference = preference;
+            this.language = language;
         }
     }
 
     class Male extends Profile
     {
-        constructor(imgsrc,name)
+        constructor(imgsrc,name,language)
         {
 
-           super(imgsrc,name); 
+           super(imgsrc,name,language); 
             this.gender = "male";
         }
 
@@ -24,10 +24,10 @@ $(document).ready(function() {
 
     class Female extends Profile
     {
-        constructor(imgsrc,name)
+        constructor(imgsrc,name,language)
         {
 
-           super(imgsrc,name); 
+           super(imgsrc,name,language); 
             this.gender = "female";
         }
 
@@ -35,10 +35,10 @@ $(document).ready(function() {
 
     class Other extends Profile
     {
-        constructor(imgsrc,name)
+        constructor(imgsrc,name,language)
         {
 
-           super(imgsrc,name); 
+           super(imgsrc,name,language); 
            this.gender = "other";
         }
 
@@ -54,25 +54,43 @@ $(document).ready(function() {
 
     //females
     var females = [];
-    var riko = new Female("../Assets/profiles/riko.png", "Riko");
+    var riko = new Female("../Assets/profiles/riko.png", "Riko","python");
+    var ozen = new Female("../Assets/profiles/ozen.png", "Ozen","java");
+    var lyza = new Female("../Assets/profiles/lyza.png","Lyza","javascript");
+    var prushka = new Female("../Assets/profiles/prushka.png","Prushka","C");
     females.push(riko);
-    var ozen = new Female("../Assets/profiles/ozen.png", "Ozen");
     females.push(ozen);
+    females.push(lyza);
+    females.push(prushka);
+
+
 
     //males
     var males = [];
-    var reg = new Male("../Assets/profiles/reg.png", "Reg");
+    var reg = new Male("../Assets/profiles/reg.png", "Reg","python");
+    var habo = new Male("../Assets/profiles/habo.png", "Habo","java");
+    var jirou = new Male("../Assets/profiles/jirou.png", "Jirou","javascript");
+    var bondrewd = new Male("../Assets/profiles/bondrewd.png", "Bondrewd","C");
     males.push(reg);
-    var habo = new Male("../Assets/profiles/habo.png", "Habo");
     males.push(habo);
+    males.push(jirou);
+    males.push(bondrewd);
+
 
     //others
     var others = [];
-    var nanachi = new Other("../Assets/profiles/nanachi.png", "Nanachi");
+    var nanachi = new Other("../Assets/profiles/nanachi.png", "Nanachi","python");
+    var maruruk = new Other("../Assets/profiles/maruruk.png", "Maruruk","java");
+    var faputa = new Other("../Assets/profiles/faputa.png", "Faputa","javascript");
+    var mitty = new Other("../Assets/profiles/mitty.png", "Mitty","C");
     others.push(nanachi);
+    others.push(maruruk);
+    others.push(faputa);
+    others.push(mitty);
 
     //form controller
     
+   
     $("#matchmaker").find("form").change(function()
     {
         //add 3 function booleans that return true if they have been filled
@@ -131,20 +149,102 @@ $(document).ready(function() {
         
         if (isGenderChecked && isNameChecked && isAgeChecked && isLangChecked)
         {
-            $("#matchmaker").find("button").prop('disabled',false);   
+            $("#matchmaker").find("button").prop('disabled',false);
         }
         else
         {
-            $("#matchmaker").find("button").prop('disabled',true);  
+            $("#matchmaker").find("button").prop('disabled',true);
+        }
+      
+        
+    });
+    
+    $("#matchmaker").find("button").click(function()
+    {
+        var name = $("#matchmaker").find("form").find("input[id='name']").val();
+        var age = $("#matchmaker").find("form").find("input[id='age']").val();
+        var gender;
+        var language;
+        
+        if($("#matchmaker").find("form").find("#male").prop('checked'))
+        {
+            gender = "male";
+        }
+        else if ($("#matchmaker").find("form").find("#female").prop('checked'))
+        {
+            gender = "female";
+        }
+        else
+        {
+            gender = "other";
         }
         
+        //language
+        if($("#matchmaker").find("form").find("#python").prop('checked'))
+        {
+            language = "python";
+        }
+        else if ($("#matchmaker").find("form").find("#java").prop('checked'))
+        {
+            language = "java";
+        }
+        else if ($("#matchmaker").find("form").find("#javascript").prop('checked'))
+        {
+            language = "javascript";
+        }
+        else
+        {
+            language = "C";
+        }
+        
+        var userInfo = new UserResponse(gender, name, age, language);
        
-
+        
+        var pickedGenderArray;
+        
+        if (userInfo.gender == "male")
+        {
+            pickedGenderArray = females.slice();
+        }
+        else if (userInfo.gender == "female")
+        {
+            pickedGenderArray = males.slice();
+        }
+        else
+        {
+            pickedGenderArray = others.slice();
+        }
+        
+        var matchProfile;
+        for(i = 0; i<pickedGenderArray.length;i++)
+        {
+            if(pickedGenderArray[i].language == userInfo.language)
+            {
+                
+                 matchProfile = Object.assign({},pickedGenderArray[i]);  
+            }
+        }
+        
+        $("#match-profile").find("#meassage").text( `Hi ${userInfo.name} , You have been matched with`);
+        $("#match-profile").find("img").attr("src",matchProfile.imagesrc);
+        $("#match-profile").find("#name").text( matchProfile.name);
+        
+        
+        
+        
+        
+        
+        
     });
+    
+    
+    //submit controller
+    
+    
     
     //PseudoCode
     //add eventlistener to the form
-    //  -checks if everything is filled
+    //  -checks if everything is filled  ----------------------------------completeed---------------
     //  -if so, unwraps the submit button to enable it
     
     //add even listener to the submit button 
